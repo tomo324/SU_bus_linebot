0bf2421fb30ed3a69050c4e176da8c175fb54475
tomo324 <0204tomo.mc@gmail.com>
2023-03-24 01:41:38 +0900

    add env

---
+++ variable.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ["CHANNEL_ACCESS_TOKEN"] = "bGuPMcxXXL2q5aT0bdrJsGxqU00E3yEteLm3LI0JoiiCn+XlfXj8vmdMiaWKDScDKThNhASwARzoZJcuOBKzBN/WuG0vh6pjJxbn5BqU4wn2HapgFj9G6bwmQUZEL4IcYTiBUK4H/SF43X7F3oYTygdB04t89/1O/w1cDnyilFU="
+os.environ["USER_ID"] = "Ubea97805839d8e4427138ad7cce1a869"
+
+#環境変数に格納。非公開
