cfdcbfe5e181189333a426ea9c9204f7744f230e
tomo324 <0204tomo.mc@gmail.com>
2023-03-26 22:02:20 +0900

    20230326

---
+++ main.py
@@ -0,0 +1,28 @@
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+import settings
+import bus_info
+
+line_bot_api = LineBotApi(settings.CHANNEL_ACCESS_TOKEN)
+
+kokusai_info = bus_info.KokusaiScraper()
+kokusai_result = kokusai_info.scrape()
+
+seibu_info = bus_info.SeibuScraper()
+seibu_result = seibu_info.scrape()
+
+text = ""
+
+def main():
+    messages = TextSendMessage(text)
+    line_bot_api.push_message(settings.USER_ID, messages=messages)
+
+if __name__ == "__main__":
+    if kokusai_result is not None:
+        if int(kokusai_result) > 5:
+            text += "国際興業バス:" + kokusai_result + "分の遅れ" + "\n"
+    if seibu_result is not None:
+        if int(seibu_result) > 18:
+            text += "西武バス所要時間:" + seibu_result + "分"
+    if text:
+        main()
